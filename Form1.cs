ba8a5a65cb6287888b6519ea2d4a0a780bf11e79
vernusotku <aleksandr.popenov300@yandex.ru>
2024-02-21 20:54:58 +0300

    - Added the System Metric class to get information about the computer. - Added an empty SystemTime object to extract the time under WINAPI GetLocalTime... P.S. The most important thing is not to get stupid.

--- Form1.cs
+++ Form1.cs
@@ -4,29 +4,30 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
+using static WindowsFormsApp3.SystemMetric;
 
 namespace WindowsFormsApp3
 {
 
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll", EntryPoint = "GetSystemMetrics")]
-        public static extern int GetSystemMetrics(int nIndex);
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private SystemMetric sm = new SystemMetric();
+/// TODO:
+/// GetLocalTime: Создать класс и извлечь фигни https://learn.microsoft.com/en-us/previous-versions/aa915317(v=msdn.10)
+/// GetSystemlnfo: понять и простить :с (https://learn.microsoft.com/ru-ru/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsysteminfo)
+/// GetSysColor or SetSysColor:(Ой бл..........) https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getsyscolor 
 
-        public enum SystemMetric : int
-        {
-            SM_CXBORDER = 5,
-        }
+
 
 
 
@@ -42,7 +43,9 @@ namespace WindowsFormsApp3
                 case "GetVersionEx":
                     return String.Format("Версия ОС: {0}",System.Environment.OSVersion);
                 case "GetSystemMetrics":
-                    return GetSystemMetrics(SystemMetric.SM_CXBORDER).ToString();
+                    return sm.GetInfo();
+                case "GetLocalTime":
+                    return "Ну и хуйня твое время...";
                 default:
                     return "000";
             };
---
+++ SystemTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp3
+{
+
+    public class SystemTime
+    {
+        [DllImport("Kernel32.dll")]
+        public static extern void GetLocalTime(ref SystemTime lpSystemTime);
+    }
+}
